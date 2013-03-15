f2c76ef699a17191a39a82800c430eb148a8771f
Håvard Fossli <hfossli@gmail.com>
2013-03-15 19:02:54 +0100

    More to come

---
+++ Source/CATransform3D+Extra.m
@@ -0,0 +1,17 @@
+//
+//  CATransform3D+Extra.m
+//  AGGeometryKit
+//
+//  Created by Håvard Fossli on 15.03.13.
+//  Copyright 2013 Håvard Fossli. All rights reserved.
+//
+
+#import "CATransform3D+Extra.h"
+
+
+// Soooo many juicy bits to be found here http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/2.2_r1.1/android/opengl/Matrix.java
+
+CATransform3D CATransform3DForRectToRect(CGRect r1, CGRect r2)
+{
+    
+}
