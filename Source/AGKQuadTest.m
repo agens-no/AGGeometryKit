4de3f3b9b1d2b7b43c69dece878176bd134327a9
Håvard Fossli <hfossli@gmail.com>
2014-06-30 12:21:33 +0200

    Writing tests for new pull request and other functions

--- Source/AGKQuadTest.m
+++ Source/AGKQuadTest.m
@@ -125,10 +125,109 @@
 
 - (void)testAGKQuadGetCenter
 {
+    AGKQuad q = AGKQuadMakeWithCGSize(CGSizeMake(500, 500));
+    CGPoint center = AGKQuadGetCenter(q);
+    XCTAssertEqualObjects([NSValue valueWithCGPoint:center], [NSValue valueWithCGPoint:CGPointMake(250, 250)]);
+}
+
+- (void)testAGKQuadGetBoundingRect
+{
+    AGKQuad q = AGKQuadMake(CGPointMake(5, 5), CGPointMake(301, -101), CGPointMake(500, 600), CGPointMake(-102, 402));
+    CGRect boundingRect = AGKQuadGetBoundingRect(q);
+    CGRect expected = CGRectMake(-102, -101, 500+102, 600+101);
+    XCTAssertEqualObjects([NSValue valueWithCGRect:expected], [NSValue valueWithCGRect:boundingRect]);
+}
+
+- (void)testAGKQuadInsetTop
+{
+    AGKQuad q = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    q = AGKQuadInsetTop(q, 0.5);
+    AGKQuad expected = AGKQuadMake(CGPointMake(1, 1.5), CGPointMake(2, 1.5), CGPointMake(2, 2), CGPointMake(1, 2));
+    XCTAssertTrue(AGKQuadEqual(q, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(q), NSStringFromAGKQuad(expected));
+}
+
+- (void)testAGKQuadInsetRight
+{
+    AGKQuad q = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    q = AGKQuadInsetRight(q, 0.5);
+    AGKQuad expected = AGKQuadMake(CGPointMake(1, 1), CGPointMake(1.5, 1), CGPointMake(1.5, 2), CGPointMake(1, 2));
+    XCTAssertTrue(AGKQuadEqual(q, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(q), NSStringFromAGKQuad(expected));
+}
+
+- (void)testAGKQuadInsetBottom
+{
+    AGKQuad q = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    q = AGKQuadInsetBottom(q, 0.5);
+    AGKQuad expected = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 1.5), CGPointMake(1, 1.5));
+    XCTAssertTrue(AGKQuadEqual(q, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(q), NSStringFromAGKQuad(expected));
+}
+
+- (void)testAGKQuadInsetLeft
+{
+    AGKQuad q = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    q = AGKQuadInsetLeft(q, 0.5);
+    AGKQuad expected = AGKQuadMake(CGPointMake(1.5, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1.5, 2));
+    XCTAssertTrue(AGKQuadEqual(q, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(q), NSStringFromAGKQuad(expected));
+}
+
+- (void)testAGKQuadMoveAndInterpolate
+{
+    AGKQuad q1 = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    AGKQuad q2 = AGKQuadMove(q1, 1, -1);
     {
-        AGKQuad q = AGKQuadMakeWithCGSize(CGSizeMake(500, 500));
-        CGPoint center = AGKQuadGetCenter(q);
-        XCTAssertEqualObjects([NSValue valueWithCGPoint:center], [NSValue valueWithCGPoint:CGPointMake(250, 250)]);
+        AGKQuad interpolated = AGKQuadInterpolate(q1, q2, 0.0);
+        AGKQuad expected = q1;
+        XCTAssertTrue(AGKQuadEqual(interpolated, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(interpolated), NSStringFromAGKQuad(expected));
+    }
+    {
+        AGKQuad interpolated = AGKQuadInterpolate(q1, q2, 0.5);
+        AGKQuad expected = AGKQuadMove(q1, 0.5, -0.5);
+        XCTAssertTrue(AGKQuadEqual(interpolated, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(interpolated), NSStringFromAGKQuad(expected));
+    }
+    {
+        AGKQuad interpolated = AGKQuadInterpolate(q1, q2, 1.0);
+        AGKQuad expected = q2;
+        XCTAssertTrue(AGKQuadEqual(interpolated, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(interpolated), NSStringFromAGKQuad(expected));
+    }
+    {
+        AGKQuad interpolated = AGKQuadInterpolate(q1, q2, -2.0);
+        AGKQuad expected = AGKQuadMake(CGPointMake(-1, 3), CGPointMake(0, 3), CGPointMake(0, 4), CGPointMake(-1, 4));
+        XCTAssertTrue(AGKQuadEqual(interpolated, expected), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(interpolated), NSStringFromAGKQuad(expected));
+    }
+}
+
+- (void)testAGKQuadRotate
+{
+    AGKQuad q1 = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    {
+        AGKQuad rotated = AGKQuadRotate(q1, 0);
+        AGKQuad expected = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+        XCTAssertTrue(AGKQuadEqualWithAccuracy(rotated, expected, 0.01), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(rotated), NSStringFromAGKQuad(expected));
+    }
+    {
+        AGKQuad rotated = AGKQuadRotate(q1, M_PI_2);
+        AGKQuad expected = AGKQuadMake(CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2), CGPointMake(1, 1));
+        XCTAssertTrue(AGKQuadEqualWithAccuracy(rotated, expected, 0.01), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(rotated), NSStringFromAGKQuad(expected));
+    }
+    {
+        AGKQuad rotated = AGKQuadRotate(q1, M_PI_4);
+        AGKQuad expected = AGKQuadMake(CGPointMake(1.5, 0.79), CGPointMake(2.20, 1.5), CGPointMake(1.5, 2.20), CGPointMake(0.79, 1.5));
+        XCTAssertTrue(AGKQuadEqualWithAccuracy(rotated, expected, 0.01), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(rotated), NSStringFromAGKQuad(expected));
+    }
+}
+
+- (void)testAGKQuadRotateAroundPoint
+{
+    AGKQuad q1 = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+    {
+        AGKQuad rotated = AGKQuadRotateAroundPoint(q1, CGPointMake(100, 100), 0);
+        AGKQuad expected = AGKQuadMake(CGPointMake(1, 1), CGPointMake(2, 1), CGPointMake(2, 2), CGPointMake(1, 2));
+        XCTAssertTrue(AGKQuadEqualWithAccuracy(rotated, expected, 0.01), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(rotated), NSStringFromAGKQuad(expected));
+    }
+    {
+        AGKQuad rotated = AGKQuadRotateAroundPoint(q1, CGPointMake(11, 0), M_PI_2);
+        AGKQuad expected = AGKQuadMake(CGPointMake(10, -10), CGPointMake(10, -9), CGPointMake(9, -9), CGPointMake(9, -10));
+        XCTAssertTrue(AGKQuadEqualWithAccuracy(rotated, expected, 0.01), @"Quad is not equal. Got %@. Expected: %@", NSStringFromAGKQuad(rotated), NSStringFromAGKQuad(expected));
     }
 }
 
