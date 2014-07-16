2ce9a5b2763389ec6b3780836f72a1df0aa2efe1
Logan Holmes <snown27@gmail.com>
2014-07-16 15:11:55 -0700

    Adding tests for "Counting Entries" and some of "Accessing Members, Columns and Rows"

--- Source/Classes/AGKMatrixTest.m
+++ Source/Classes/AGKMatrixTest.m
@@ -13,6 +13,9 @@
 @end
 
 @interface AGKMatrix ()
+@property (nonatomic, assign) NSUInteger columnCount;
+@property (nonatomic, assign) NSUInteger rowCount;
+
 @property (nonatomic, strong) NSMutableArray *members;
 @end
 
@@ -70,6 +73,86 @@
     XCTAssertEqualObjects(matrix, sameMatrixAgain, @"Matrices should be equal");
     XCTAssertNotEqualObjects(matrix, differentMatrix, @"Matrices should not be equal");
     
+}
+
+- (void)testMatrixColumnCount
+{
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:4 rows:3 members:members];
+    
+    XCTAssertEqual(matrix.columnCount, 4, @"Column count should be 4 regardless of how many members have been specified.");
+}
+
+- (void)testMatrixRowCount
+{
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    XCTAssertEqual(matrix.rowCount, 4, @"Row count should be 4 regardless of how many members have been specified.");
+}
+
+- (void)testMatrixTotalCount
+{
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3, @4, @4, @3, @2, @1]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    XCTAssertEqual(matrix.count, (3 * 4), @"Count should be columns times rows, or 12 in the case of a 3x4 matrix.");
+    
+    matrix.columnCount += 1;
+    XCTAssertEqual(matrix.count, (4 * 4), @"Count should return the total number of spots in the matrix based on the specified row and column dimensions.");
+}
+
+- (void)testObjectAtColumnIndexRowIndex
+{
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3, @4, @5, @6, @7, @8]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    NSNumber *member = [matrix objectAtColumnIndex:1 rowIndex:1];
+    XCTAssertEqualObjects(member, @6, @"Column/Row indexes are not returning the correct member");
+}
+
+- (void)testColumns {
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3, @4, @5, @6, @7, @8]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    NSArray *columns = matrix.columns;
+    NSArray *comparisonArray = @[@[@1, @2, @3, @4], @[@5, @6, @7, @8], @[@0, @0, @0, @0]];
+    
+    XCTAssertEqual(columns.count, comparisonArray.count, @"Columns method is not returning the correct number of column arrays.");
+    for (NSUInteger index = 0; index < columns.count; index++) {
+        XCTAssert([(NSArray *)columns[index] isEqualToArray:comparisonArray[index]], @"Returned column at index %d should be: %@", index, comparisonArray[index]);
+    }
+}
+
+- (void)testRows {
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3, @4, @5, @6, @7, @8]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    NSArray *rows = matrix.rows;
+    NSArray *comparisonArray = @[@[@1, @5, @0], @[@2, @6, @0], @[@3, @7, @0], @[@4, @8, @0]];
+    
+    XCTAssertEqual(rows.count, comparisonArray.count, @"Rows method is not returning the correct number of row arrays.");
+    for (NSUInteger index = 0; index < rows.count; index++) {
+        XCTAssert([(NSArray *)rows[index] isEqualToArray:comparisonArray[index]], @"Returned row at index %d should be: %@", index, comparisonArray[index]);
+    }
+}
+
+- (void)testColumnAtIndex {
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3, @4, @5, @6, @7, @8]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    NSArray *comparisonColumn = @[@5, @6, @7, @8];
+    NSArray *column = [matrix columnAtIndex:1];
+    XCTAssert([column isEqualToArray:comparisonColumn], @"Column 1 should be %@", comparisonColumn);
+}
+
+- (void)testRowAtIndex {
+    NSMutableArray *members = [NSMutableArray arrayWithArray:@[@1, @2, @3, @4, @5, @6, @7, @8]];
+    AGKMatrix *matrix = [[AGKMatrix alloc] initWithColumns:3 rows:4 members:members];
+    
+    NSArray *comparisonColumn = @[@2, @6, @0];
+    NSArray *column = [matrix rowAtIndex:1];
+    XCTAssert([column isEqualToArray:comparisonColumn], @"Row 1 should be %@", comparisonColumn);
 }
 
 @end
