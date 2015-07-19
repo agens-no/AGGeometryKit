ef81d4c684acb2aef40a70f211462478d23fee1b
James Tang <j@jamztang.com>
2015-07-20 03:19:55 +0800

    Create AGGeometryKitiOS.framework target

---
+++ Demo/AGGeometryKitiOS/AGGeometryKitiOS.h
@@ -0,0 +1,20 @@
+//
+//  AGGeometryKitiOS.h
+//  AGGeometryKitiOS
+//
+//  Created by James Tang on 20/7/15.
+//  Copyright © 2015 H√•vard Fossli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGGeometryKitiOS.
+FOUNDATION_EXPORT double AGGeometryKitiOSVersionNumber;
+
+//! Project version string for AGGeometryKitiOS.
+FOUNDATION_EXPORT const unsigned char AGGeometryKitiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGGeometryKitiOS/PublicHeader.h>
+
+
+#import "AGGeometryKit.h"
---
+++ Demo/AGGeometryKitiOSTests/AGGeometryKitiOSTests.m
@@ -0,0 +1,39 @@
+//
+//  AGGeometryKitiOSTests.m
+//  AGGeometryKitiOSTests
+//
+//  Created by James Tang on 20/7/15.
+//  Copyright © 2015 H√•vard Fossli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AGGeometryKitiOSTests : XCTestCase
+
+@end
+
+@implementation AGGeometryKitiOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
